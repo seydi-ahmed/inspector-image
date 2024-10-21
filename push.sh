123f6854c9ed7164a964c564c334baa74177d188
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-10-21 12:06:35 +0000

    one file

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ExifRead au lieu de Stegano"
+git commit -m "one file"
 git push
